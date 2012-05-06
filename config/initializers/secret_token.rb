0f8ef96759b612ffd61a710c4cb02a5e54b9f17d
Doug Orleans <dougorleans@gmail.com>
2012-05-05 20:43:33 -0400

    FamilyTree Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyTree::Application.config.secret_token = '959d5e6ac3f46b48a25ba2b01cf0e6e41cb44c5bc9de4cfea6a84c4f7a261422e77b1f81e6ea8c87ed16a326ae5d4d058919340ae800991f024b964215b0cc91'
