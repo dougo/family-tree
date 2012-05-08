18cc6419b76ca14b4500f7ed8722ae1da970b2bd
Doug Orleans <dougorleans@gmail.com>
2012-05-07 23:19:23 -0400

    Show relationships on person view.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,4 +2,11 @@ class Person < ActiveRecord::Base
   attr_accessible :date_of_birth, :name
 
   validates :name, :presence => true
+
+  has_many :relationships
+  has_many :reverse_relationships, class_name: 'Relationship', foreign_key: 'related_person_id'
+
+  def all_relationships
+    relationships | reverse_relationships
+  end
 end
