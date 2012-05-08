263d8be428530bf93f19c35c9dfa93eb50962539
Doug Orleans <dougorleans@gmail.com>
2012-05-07 20:49:24 -0400

    Require all relationship fields.

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,6 @@ class Relationship < ActiveRecord::Base
   belongs_to :relationship_type
   belongs_to :person
   belongs_to :related_person, :class_name => 'Person'
+
+  validates :relationship_type, :person, :related_person, :presence => true
 end
--- test/unit/relationship_test.rb
+++ test/unit/relationship_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'require all fields' do
+    r = Relationship.new
+    assert !r.valid?
+    r.relationship_type = relationship_types(:one)
+    assert !r.valid?
+    r.person = people(:one)
+    assert !r.valid?
+    r.related_person = people(:two)
+    assert r.valid?
+  end
 end
